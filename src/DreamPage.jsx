ec3cf21e0a9cb12509d378cfb6a2c4f83ae8dc22
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-16 20:09:46 -0500

    Created and added the visionContainer component

--- src/DreamPage.jsx
+++ src/DreamPage.jsx
@@ -1,9 +1,11 @@
 import NavTop from "./NavTop";
+import VisionContainer from "./VisionContainer";
 
 export default function DreamPage() {
   return (
     <div className="p-4">
       <NavTop />
+      <VisionContainer />
     </div>
   );
 }
---
+++ src/VisionContainer.jsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <section className="flex flex-col gap-2">
+      <h4 className="heading4 text-greys-TITLE">Mi visión es</h4>
+      <p className="small-regular text-greys-TEXT">
+        Vivir en un departamento con balcón frente al mar de la costa verde
+        gracias al trabajo en EDteam y MMJ de manera remota con un cuerpo
+        atlético y siendo figura pública en internet y atractivo para las
+        chicas.
+      </p>
+    </section>
+  );
+}
