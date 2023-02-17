40107f37d0ae4bb8ddb8e090caacd18e8fbdccc7
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-16 20:37:34 -0500

    Creado un archivo con mis datos tipo object para simular una API

---
+++ src/assets/user.js
@@ -0,0 +1,25 @@
+var user = {
+  id: 1,
+  name: "Denilson",
+  lastname: "Trujillo Salazar",
+  alias: "DenilDenilson",
+  dreams: [
+    "Ver una película con pizza y coca cola con mi familia",
+    "Llevar a mi familia a ver las 7 maravillas del mundo",
+    "Pagar las deudas de mis padres",
+    "Terminar de construir el segundo piso y la azotea de mi casa",
+    "Comprarme la subscripción anual de EDteam",
+    "Comprarme una computadora gamer y una laptop gozu y liviana",
+    "Ir al mejor restaurante del mundo",
+    "Ir al mejor restaurante del Perú",
+    "Comprarme y comprarle ropa hermana a toda mi familia",
+    "Apoyar a Danna con un negocio, libros y educación no tradicional",
+    "Comprarme y comprarle a mi hermana un buen perfume",
+    "Comprarme un buen teléfono",
+    "Vivir en un departamento con vista al mar en Lima sur Perú",
+    "Trabajar mano a mano con Xavier Hernandez de MMJ",
+    "Trabajar con Álvaro Felipe de EDteam",
+  ],
+};
+
+export default user;
