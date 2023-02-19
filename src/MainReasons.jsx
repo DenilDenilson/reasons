85c14f1bafc8eecfbe2194b57836f3a37cfa18ff
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-19 17:24:04 -0500

    Añadidos los links a diferentes páginas

--- src/MainReasons.jsx
+++ src/MainReasons.jsx
@@ -3,7 +3,7 @@ import VisionContainer from "./VisionContainer";
 
 export default function MainReasons() {
   return (
-    <main className="px-4 pt-14 pb-24">
+    <main className="px-4 pt-14 pb-24 flex flex-col gap-4">
       <VisionContainer />
       <ReasonContainer />
     </main>
