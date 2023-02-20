1b283a411d6a451e872d591872eb524f4f302e8e
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-20 18:05:43 -0500

    Añadido la logica de cambi de paginas en el navBotton

--- src/DreamPage.jsx
+++ src/DreamPage.jsx
@@ -6,13 +6,14 @@ import Add from "./assets/Add";
 import FixedButtom from "./FixedButtom";
 import MainDreams from "./MainDreams";
 import { Outlet } from "react-router-dom";
+import NavBot from "./NavBot";
 
 export default function DreamPage() {
   return (
     <div className="min-h-screen relative bg-greys-BG">
       <NavTop />
       <Outlet />
-      <NavBottom />
+      <NavBot />
       <FixedButtom />
     </div>
   );
--- src/NavBot.jsx
+++ src/NavBot.jsx
@@ -1,22 +1,79 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Cloudy from "./assets/Cloudy";
 import Favorite from "./assets/Favorite";
 import Quote from "./assets/Quote";
 
 export default function NavBot() {
+  const [filledCloudy, setFilledCloudy] = useState(true);
+  const [filledFavorite, setFilledFavorite] = useState(false);
+  const [filledQuote, setFilledQuote] = useState(false);
+
+  const spanCloudy = useRef();
+  const spanFavorite = useRef();
+  const spanQuote = useRef();
+
   return (
     <footer className="grid grid-cols-3 pt-4 pb-4 shadow-[0px_-2px_4px_rgba(98,0,107,0.25)] fixed bottom-0 w-full bg-greys-BG">
-      <Link to="/" className="flex flex-col items-center gap-1">
-        <Cloudy isFilled={true} />
-        <span className="title-small-bold text-base-500">Sueños</span>
+      <Link
+        to="/"
+        className="flex flex-col items-center gap-1"
+        onClick={() => {
+          setFilledCloudy(true);
+          setFilledFavorite(false);
+          setFilledQuote(false);
+          // Letras
+          spanCloudy.current.classList.add("text-base-500");
+          spanCloudy.current.classList.remove("text-purple-900");
+          spanFavorite.current.classList.remove("text-base-500");
+          spanFavorite.current.classList.add("text-purple-900");
+          spanQuote.current.classList.remove("text-base-500");
+          spanQuote.current.classList.add("text-purple-900");
+        }}
+      >
+        <Cloudy isFilled={filledCloudy} />
+        <span ref={spanCloudy} className="title-small-bold text-base-500">
+          Sueños
+        </span>
       </Link>
-      <Link to="/reasons" className="flex flex-col items-center gap-1">
-        <Favorite isFilled={false} />
-        <span className="title-small-bold text-purple-900">Motivos</span>
+      <Link
+        to="/reasons"
+        className="flex flex-col items-center gap-1"
+        onClick={() => {
+          setFilledFavorite(true);
+          setFilledCloudy(false);
+          setFilledQuote(false);
+          spanCloudy.current.classList.remove("text-base-500");
+          spanCloudy.current.classList.add("text-purple-900");
+          spanFavorite.current.classList.add("text-base-500");
+          spanFavorite.current.classList.remove("text-purple-900");
+          spanQuote.current.classList.remove("text-base-500");
+          spanQuote.current.classList.add("text-purple-900");
+        }}
+      >
+        <Favorite isFilled={filledFavorite} />
+        <span ref={spanFavorite} className="title-small-bold text-purple-900">
+          Motivos
+        </span>
       </Link>
-      <Link className="flex flex-col items-center gap-1">
-        <Quote isFilled={false} />
-        <span className="title-small-bold text-purple-900">Frases</span>
+      <Link
+        className="flex flex-col items-center gap-1"
+        onClick={() => {
+          setFilledQuote(true);
+          setFilledCloudy(false);
+          setFilledFavorite(false);
+          spanCloudy.current.classList.remove("text-base-500");
+          spanCloudy.current.classList.add("text-purple-900");
+          spanFavorite.current.classList.remove("text-base-500");
+          spanFavorite.current.classList.add("text-purple-900");
+          spanQuote.current.classList.add("text-base-500");
+          spanQuote.current.classList.remove("text-purple-900");
+        }}
+      >
+        <Quote isFilled={filledQuote} />
+        <span ref={spanQuote} className="title-small-bold text-purple-900">
+          Frases
+        </span>
       </Link>
     </footer>
   );
