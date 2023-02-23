e9164a81432993b18f73d7e3477e9a4bdd1e0404
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-22 22:29:17 -0500

    Agregado estilos a la quotePage

--- src/MainQuote.jsx
+++ src/MainQuote.jsx
@@ -1,7 +1,16 @@
+import QuoteRounded from "./assets/QuoteRounded";
+
 export default function MainQuote() {
   return (
     <main className="absolute left-0 right-0 top-0 bottom-0 m-auto flex items-center justify-center">
-      <p className="">"Todo ladrillo hace castillo"</p>
+      <div className="flex gap-1">
+        <QuoteRounded class="rotate-180" />
+        <p className="bigger-regular text-greys-TEXT">
+          Todo ladrillo hace castillo
+        </p>
+        <QuoteRounded />
+        {/* // Se puede usar un className también xd */}
+      </div>
     </main>
   );
 }
