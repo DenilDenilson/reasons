61e310d1c634bb545ed0181d71cfe4ed10364de0
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-22 22:59:30 -0500

    Añadido la lógica para cambiar el svg en cada página usando el useLocate del react-router

--- src/FixedButtom.jsx
+++ src/FixedButtom.jsx
@@ -1,9 +1,20 @@
+import { useLocation } from "react-router";
 import Add from "./assets/Add";
+import Shuffle from "./assets/Shuffle";
 
 export default function FixedButtom() {
+  const location = useLocation();
+
+  console.log(location);
+  console.log(location.pathname.includes("quote"));
+
   return (
     <div className="fixed bottom-28 right-4 shadow-[2px_2px_4px_rgba(31,31,31,0.5)] bg-base-600 rounded-full">
-      <Add />
+      {location.pathname.includes("quote") ? (
+        <Shuffle className="rotate-180 p-1" />
+      ) : (
+        <Add />
+      )}
     </div>
   );
 }
