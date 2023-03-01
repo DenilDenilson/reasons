69c2ae3eae404ddb1b3b88f1c6d4ee1def019505
DenilDenilson <jordanpreseleccion@gmail.com>
2023-02-28 21:45:00 -0500

    Terminado y añadido funcionalidad al active state de dreamAdd

--- src/ModalMore.jsx
+++ src/ModalMore.jsx
@@ -1,11 +1,15 @@
+import Close from "./assets/Close";
 import SecondaryButton from "./assets/SecondaryButton";
 import DreamSelection from "./DreamSelection";
 
-export default function ModalMore({ text }) {
+export default function ModalMore({ text, closeModel }) {
   return (
     <section className="bg-greys-BG w-80 flex flex-col gap-4 absolute top-40 left-0 right-0 m-auto p-4 shadow-xl rounded-lg">
       <p className="heading4 text-greys-TITLE flex justify-between">
-        {text} <span>🌶️</span>
+        {text}
+        <span onClick={closeModel}>
+          <Close className="bg-base-500 rounded-full" />{" "}
+        </span>
       </p>
       <textarea
         className="placeholder-greys-BORDER placeholder:small-regular border-[1px] border-greys-BORDER h-24 p-2 rounded-lg border-opacity-50 small-regular text-greys-TEXT outline-1 focus:outline-base-500"
