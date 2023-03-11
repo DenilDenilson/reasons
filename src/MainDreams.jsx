f2b79444b685d823ffdf5642170795e2a7990813
DenilDenilson <jordanpreseleccion@gmail.com>
2023-03-11 13:15:23 -0500

    modifier MainDreams

--- src/MainDreams.jsx
+++ src/MainDreams.jsx
@@ -1,4 +1,5 @@
 import DreamContainer from "./DreamContainer";
+import NavTopOptions from "./NavTopOptions";
 import VisionContainer from "./VisionContainer";
 
 export default function MainDreams() {
@@ -6,6 +7,7 @@ export default function MainDreams() {
     <main className="px-4 pt-14 pb-24 flex flex-col gap-4">
       <VisionContainer />
       <DreamContainer />
+      <NavTopOptions />
     </main>
   );
 }
